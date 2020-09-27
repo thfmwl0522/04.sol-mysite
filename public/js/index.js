efa3bca9baac9055a0099008dbe7b7a8ca6f6f97
thfmwl0522 <thfmwl0522@naver.com>
2020-09-28 00:12:40 +0900

    9/27 수정

--- public/js/index.js
+++ public/js/index.js
@@ -8,65 +8,71 @@ function onLeave() {
 	$(this).find(".sub-wrap").stop().slideUp(500);
 }
 
-/* (function(){
-	var slides = [
-		{ id: 0, src: '../img/slide-01.jpg', title: '명화1' },
-		{ id: 1, src: '../img/slide-02.jpg', title: '명화2' },
-		{ id: 2, src: '../img/slide-03.jpg', title: '명화3' },
-		{ id: 3, src: '../img/slide-04.jpg', title: '명화4' }
-	];
-
-	var $slideWrap = $(".header-wrapper .slide-wrap");
+(function(){
+	var $stage = $(".header-wrapper .stage");
+	var $wrapper = $(".header-wrapper .slide-wrapper");
+	var $slides = $(".header-wrapper .slide");
+	var $titleLt = $(".header-wrapper .title-lt"); 
+	var $titles = $(".header-wrapper .title-lt .title-wrap");
 	var $btnPrev = $(".header-wrapper .btn-prev");
 	var $btnNext = $(".header-wrapper .btn-next");
 	var $pagerWrap = $(".header-wrapper .pager-wrap");
-	var $pager;
-	var $slides = [];
 	var idx = 0;
-	var lastIdx = slides.length - 1;
+	var lastIdx = $slides.length - 1;
 	var interval;
-
+	init();
+	
 	function init() {
-		var html, i;
-		for(i in slides) {
-			html = '<div class="slide">';
-			html += '<img class="w-100" src="'+slides[i].src+'">';
-			html += '</div>';
-			$slides[i] = $(html);
-			html = '<span class="pager">●</span>';
-			$pagerWrap.append(html);
+		for(var i=0; i<$slides.length; i++) {
+			$pagerWrap.append('<div class="pager">·</div>');
 		}
-		$pager = $pagerWrap.find(".pager");
-		$pager.click(onPagerClick).eq(idx).addClass("active");
+		$pagerWrap.find(".pager").eq(idx).addClass("active");
+		$pagerWrap.find(".pager").click(onClick);
+		interval = setInterval(onInterval, 10000);
 		slideInit();
-		interval = setInterval(onNext, 10000);
 	}
 
 	function slideInit() {
-		$slideWrap.html($slides[idx].clone());
+		$(".header-wrapper .slide").remove();
+		$(".header-wrapper .title-lt .title-wrap").remove();
+		$($($slides[idx]).clone()).appendTo($wrapper);
+		$($($titles[idx]).clone()).prependTo($titleLt);
+		$(".header-wrapper .title-lt .title-wrap").css("opacity");
+		$(".header-wrapper .title-lt .title-wrap").css("transform");
+		$(".header-wrapper .title-lt .title-wrap").css({"opacity": 1, "transform": "translateY(0)"});
 	}
 
 	$btnPrev.click(onPrev);
 	$btnNext.click(onNext);
 
 	function onPrev() {
+		target = '100%';
 		idx = (idx == 0) ? lastIdx : idx - 1;
 		ani();
 	}
 
 	function onNext() {
+		target = '-100%';
 		idx = (idx == lastIdx) ? 0 : idx + 1;
 		ani();
 	}
 
-	function onPagerClick() {
+	function onClick() {
 		idx = $(this).index();
 		ani();
 	}
 
-	function ani() {
-		$pager.removeClass("active").eq(idx).addClass("active");
-		$($slides[idx].clone()).appendTo($slideWrap).stop().animate({"opacity": 1}, 500, slideInit);
+	function onInterval() {
+		idx = (idx == lastIdx) ? 0 : idx + 1;
+		ani();
 	}
-	init();
-})(); */
+
+	function ani() {
+		$pagerWrap.find(".pager").removeClass("active");
+		$pagerWrap.find(".pager").eq(idx).addClass("active");
+		$(".header-wrapper .title-lt .title-wrap").stop().animate({"opacity": 0}, 300);
+		$($($slides[idx]).clone()).appendTo($wrapper).stop().animate({"opacity": 1}, 500, slideInit);
+	}
+
+})();
+ 
